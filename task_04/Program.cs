90f663cb41853d7bb84275ca9d0c75463d6745d1
Evgeny <buriy-anarchist@inbox.ru>
2022-07-05 17:04:33 +0300

    Add task

---
+++ task_04/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 4;
+int b = 7;
+int c = 10;
+int max = a;
+if (b > max)
+{
+    Console.WriteLine("max = b");
+}
+else
+{
+    Console.WriteLine("max = a");
+}
+if (c > max)
+{
+    Console.WriteLine("max = c");
+}
+else
+{
+    Console.WriteLine("max = a");
+}
