aab55fb1d03d7acedb07100b2254dda10d8544ea
Evgeny <buriy-anarchist@inbox.ru>
2022-07-06 15:09:20 +0300

    Add task 04

--- task_04/Program.cs
+++ task_04/Program.cs
@@ -1,6 +1,7 @@
-﻿int a = 4;
-int b = 7;
-int c = 10;
+﻿int a, b, c;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 if (b > max) max = b;
 if (c > max) max = c;
